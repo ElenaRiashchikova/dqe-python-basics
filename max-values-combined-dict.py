b28d4807d504cb1b34e114ed0ad75f3ed57316e6
ElenaRiashchikova <Elena_Riashchikova@epam.com>
2026-04-21 17:06:29 +0200

    task 2

--- max-values-combined-dict.py
+++ max-values-combined-dict.py
@@ -1,50 +1,44 @@
 import random
 
 runs = 4
-dictionaries:list = []
-dictionary_keys:list = []
+dictionaries: list = []
+dictionary_keys: list = []
 final_dictionary: dict = {}
 
 for run in range(0, runs):
-    dictionary:dict = {}
-    for i in random.sample("abcdefghi", k=runs):
-        dictionary_keys.append(i)
-        dictionary[i] = random.randint(0, 100)
+    dictionary: dict = {}
+    for random_key in random.sample("abcdefghi", k=runs):
+        dictionary_keys.append(random_key)
+        dictionary[random_key] = random.randint(0, 100)
     dictionaries.append(dictionary)
 
-print(dictionaries)
+print("source_dictionaries_list:", dictionaries)
 
 dictionary_keys_set = set(dictionary_keys)
-print(dictionary_keys_set)
 
 pre_final_dictionary: list = []
 
-for i in dictionary_keys_set:
+for random_key in dictionary_keys_set:
     c: dict = {}
     pre_final_dictionary.append(c)
     for run in range(0, runs):
         dictionary: dict = dictionaries[run]
-        d = str(i) + "_" + str(run)
-        if i in dictionary.keys():
-            c.update({d: dictionary.get(i)})
+        pre_key = str(random_key) + "_" + str(run)
+        if random_key in dictionary.keys():
+            c.update({pre_key: dictionary.get(random_key)})
 
-print(pre_final_dictionary)
-
+print("pre_final_dictionary:", pre_final_dictionary)
 
 for s in pre_final_dictionary:
     if len(s) > 1:
-        t = None
-        p = None
-        for k,v in s.items():
-            if t is None or v > p:
-                t = k
-                p = v
-                # final_dictionary.update({k: v})
-        final_dictionary.update({t: p})
+        final_key = None
+        final_value = None
+        for k, v in s.items():
+            if final_key is None or v > final_value:
+                final_key = k
+                final_value = v
+        final_dictionary.update({final_key: final_value})
     else:
         final_dictionary.update(s)
 
-print(final_dictionary)
-
-
-
+print("final_dictionary:",final_dictionary)
